7cffd30b8749f61d5e3682d9c5d112b44eb61386
Romain Menke <romainmenke@gmail.com>
2015-10-02 23:21:47 +0200

    Initial Commit

---
+++ NSWindowControllerTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSWindowControllerTutorial
+//
+//  Created by Romain Menke on 02/10/15.
+//  Copyright © 2015 menke dev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
